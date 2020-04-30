854460a36f7d663d6ac951c3406cea008d043be2
fteve96 <ftevenden1@gmail.com>
2020-04-30 12:27:39 -0800

    fixed typo

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://build_tech@localhost/build_tech',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '1h'
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:5000'
 }
